4b72747e8790d64c369a08a88a0e4cd226f3625a
Fabionu <maroonyelnats@yahoo.com>
2026-03-26 09:36:46 +0100

    fixes

--- src/components/ChatPanel.jsx
+++ src/components/ChatPanel.jsx
@@ -177,7 +177,7 @@ function BackBtn({ onClick }) {
       onMouseEnter={e => e.currentTarget.style.color = 'var(--black)'}
       onMouseLeave={e => e.currentTarget.style.color = 'var(--gray-4)'}>
       <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
-        <polyline points="15 18 9 12 15 6"/>
+        <polyline points="6 15 12 9 18 15"/>
       </svg>
     </button>
   );
